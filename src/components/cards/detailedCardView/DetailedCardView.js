fba9e3ca836169dd9ac3a27fb00c3f96a2933d02
diogovsmartins <diogodmartins@rede.ulbra.br>
2023-12-02 02:48:53 -0300

    Implemented Logic to download the PDF

--- src/components/cards/detailedCardView/DetailedCardView.js
+++ src/components/cards/detailedCardView/DetailedCardView.js
@@ -1,20 +1,25 @@
-import Card from "../Card";
+import { saveAs } from "file-saver";
 import "../detailedCardView/DetailedCardView.css"
-export default function DetailedCardView({document}){
+export default function DetailedCardView({pdf}){
 
-    console.log("document from inside detailed card view:", document);
+    async function downloadPdf(){
+        const fetchPdf= await fetch(`http://localhost:8080/pdf/${pdf.id}`);
+        const responsePdf = await fetchPdf.blob();
+        console.log('test', `http://localhost:8080/pdf/${pdf.id}`)
+        saveAs(responsePdf, 'generatedDeclaration.pdf');
+    }
 
     return (
         <div id="detailed-card-view-style">
             <div id="image-div">
                 <img
                 id="image-style" 
-                src={`data:image/png;base64,${document.base64Image}`}
+                src={`data:image/png;base64,${pdf.base64Image}`}
                 alt="Portrait of the generated presenciality declaration."
                 />
             </div>
             <div id="details-style-div">
-                <button id="download-button-style">Download</button>
+                <button id="download-button-style" onClick={downloadPdf}>Download</button>
             </div>
         </div>
     );
--- src/pages/documents/SingleDocumentPage.js
+++ src/pages/documents/SingleDocumentPage.js
@@ -4,17 +4,15 @@ import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import DetailedCardView from "../../components/cards/detailedCardView/DetailedCardView";
 
-
 export default function SinglePageDocument(){
 
     const locationData=useLocation();
-
 
     return(
        <>
        <Header />
        <Navbar />
-        <DetailedCardView document={locationData.state} />
+        <DetailedCardView pdf={locationData.state} />
        <Footer />
        </> 
     );
